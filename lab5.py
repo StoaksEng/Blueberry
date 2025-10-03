d540a6762ca54e9e2d67ab16419b4dabdba3c00a
StoaksEng <StoaksEng@gmail.com>
2025-10-03 14:20:13 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -3,23 +3,27 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 
-ports = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+pins = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 but = 12
-pwms = [None] * len(ports)
 fbase = 500
 
 GPIO.setup(but, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
-for i in range(len(ports)):
-	GPIO.setup(ports[i], GPIO.OUT, initial=0)
-	print(i)
+for pin in pins
+	GPIO.setup(pin, GPIO.OUT)
+	
+pwms = [GPIO.PWM(pin, fbase) for pin in pins]
 
-for i in range(len(ports)):
-	pwms[i] = GPIO.PWM(ports[i], fbase)
-	# pwms[i].start(10*i)
+
+i=0;
+
+for pwm in pwms:
+	pwm.start(i*pi/11)
+	i+=1
 
 try:
+
 	while 1:
 		print("ping")
 		sleep(1)
@@ -27,6 +31,9 @@ except KeyboardInterrupt:
 	print('\nExiting')
 except Exception as e:
 	print('\ne')
-
-GPIO.cleanup()
+finally:
+	for pwm in pwms:
+		pwm.stop()
+	GPIO.cleanup()
+	print("done")
 
