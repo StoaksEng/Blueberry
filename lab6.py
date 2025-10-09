a04c14780c498f262f53d216957d3b1b2402b05b
StoaksEng <StoaksEng@gmail.com>
2025-10-09 15:28:05 -0400

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -15,16 +15,24 @@ pattern = 0b01100110 # pattern to display
 
 shift = shifter.Shifter(dataPin, clockPin, latchPin)
 
-dir = 1
+leds = 0b00010000 
+
+def randomStep():
+	move = random.choice([-1, 1])
+	# Move left
+    if move == -1 and leds < 0b10000000:
+        leds <<= 1
+    # Move right
+    elif move == 1 and leds > 0b00000001:
+        leds >>= 1
+    # If at edge, reverse direction
+    else:
+        leds = leds  # stay put or could reverse
 
 try:
 	while 1:
+		shift.ShiftByte(leds)
+		sleep(0.05)
 		print("test")
-		dir = random.randint(1, 2)
-		print("test")
-		shift.ShiftByte(dir)
-		print("test")
-		sleep(1)
-
 except:
 	GPIO.cleanup()
