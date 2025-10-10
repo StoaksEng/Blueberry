4334a9b2879de9f4b1c98af37e3355682b33be81
StoaksEng <StoaksEng@gmail.com>
2025-10-10 12:53:38 -0400

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -20,62 +20,62 @@ led = 0b00010000
 
 
 def randomStep(leds):
-    try:
-        bug = Bug()
-        bug.start()
+	try:
+		bug = Bug()
+		bug.start()
 
-        while True:
-            bug.start()   # starts blinking LEDs
-            time.sleep(10)
-            bug.stop()
-            time.sleep(10)
+		while True:
+			bug.start()   # starts blinking LEDs
+			time.sleep(10)
+			bug.stop()
+			time.sleep(10)
 
-    except Exception as e:
-        print("Error:", e)
-        GPIO.cleanup()
+	except Exception as e:
+		print("Error:", e)
+		GPIO.cleanup()
 
 
 class Bug:
-    def __init__(self, timeStep=0.1, x=3, isWrapOn=False):
-        # composition: Bug "has a" Shifter
-        self.shifter = shifter.Shifter(dataPin, clockPin, latchPin)
-        self.timeStep = timeStep
-        self.x = x
-        self.isWrapOn = isWrapOn
-        self.running = False
-        self._thread = None
+	def __init__(self, timeStep=0.1, x=3, isWrapOn=False):
+		# composition: Bug "has a" Shifter
+		self.shifter = shifter.Shifter(dataPin, clockPin, latchPin)
+		self.timeStep = timeStep
+		self.x = x
+		self.isWrapOn = isWrapOn
+		self.running = False
+		self._thread = None
 
-    def _run(self):
-        while self.running:
-            self.randomStep()
-            time.sleep(self.timeStep)
+	def _run(self):
+		while self.running:
+			self.randomStep()
+			time.sleep(self.timeStep)
 
-    def randomStep(self):
-        move = random.choice([-1, 1])
+	def randomStep(self):
+		move = random.choice([-1, 1])
 
-        # Move left (if possible)
-        if move == -1:
-            if self.x > 0 or self.isWrapOn:
-                self.x -= 1
+		# Move left (if possible)
+		if move == -1:
+			if self.x > 0 or self.isWrapOn:
+				self.x -= 1
 
-        # Move right (if possible)
-        elif move == 1:
-            if self.x < 7 or self.isWrapOn:
-                self.x += 1
+		# Move right (if possible)
+		elif move == 1:
+			if self.x < 7 or self.isWrapOn:
+				self.x += 1
 
-        led = 2 ** self.x
-        self.shifter.shiftByte(led)
+		led = 2 ** self.x
+		self.shifter.shiftByte(led)
 
-    def start(self):
-        if not self.running:
-            self.running = True
-            self._thread = threading.Thread(target=self._run)
-            self._thread.start()
-            print("Bug released.")
+	def start(self):
+		if not self.running:
+			self.running = True
+			self._thread = threading.Thread(target=self._run)
+			self._thread.start()
+			print("Bug released.")
 
-    def stop(self):
-        if self.running:
+	def stop(self):
+		if self.running:
 			self.running = False
-        	self._thread.join()       # wait for thread to finish
+			self._thread.join()       # wait for thread to finish
 			self.shifter.shiftByte(0)  # turn off all LEDs
 			print("Bug squahsed.")
